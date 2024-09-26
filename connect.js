7626e1420717d357786ee55f3a299bf362cee5b5
Sahil <bits.sahil@gmail.com>
2024-09-26 13:43:03 +0530

    fix

--- connect.js
+++ connect.js
@@ -94,8 +94,8 @@ let kc2 = new KiteConnect(options2);
 kc.setSessionExpiryHook(sessionHook);
 
 
-let maxPlatformLoss = 450000;  //hard limit, can't do trading after this limit
-let softMaxPlatformLoss = 280000; //soft limit
+let maxPlatformLoss = 600000;  //hard limit, can't do trading after this limit
+let softMaxPlatformLoss = 450000; //soft limit
 let softMaxPlatformLossHit = false;
 let maxPlatformLossHit = false;
 let killSwitchActivated = false;
